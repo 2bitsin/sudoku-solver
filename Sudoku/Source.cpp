1f815d715f8e084bdcacdd714aa16598e3c0357d
Aleksandras <s.aleksandras@gmail.com>
2015-08-06 23:25:19 +0300

    Mooo

--- Sudoku/Source.cpp
+++ Sudoku/Source.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <memory>
 #include <chrono>
+#include <queue>
+#include <stack>
 
 auto pre_load (std::istream& is, std::size_t n = 0u) {
     static const auto bad_format =
@@ -42,23 +44,49 @@ auto pre_load (std::istream& is, std::size_t n = 0u) {
     return out;
 }
 
+template <typename _Grid>
+auto print (const _Grid& grid) {
+    for (auto j = 0u; j < 9u; ++j) {
+        if (j % 3 == 0 && j != 0)
+            std::cout << "-------+-------+------\n";
+        for (auto i = 0u; i < 9u; ++i) {
+            std::cout << (i % 3 == 0 && i != 0 ? " | " : " ");
+            if (unsigned (grid [i + j*9u]) == 0) {
+                std::cout << ".";
+            } else {
+                std::cout << unsigned (grid [i + j*9u]);
+            }
+        }
+        std::cout << "\n";
+    }
+}
+
 struct sudoku_grid {    
     struct cell_type {
         static const auto INVALID_ALL = std::uint16_t (0x1ff);
-        cell_type               (): data (0u) {}
-        cell_type               (std::uint8_t v):data ((v&0xf) ? (v&0xf) | (INVALID_ALL << 4u) : (v&0xf)) {}
-        cell_type& operator =   (std::uint8_t v)    { return *this = cell_type (v); }
-        auto invalid            () const            { return (data >> 4u) & INVALID_ALL; }
-        auto value              () const            { return data & 0xf; }
-        auto invalid            (std::uint16_t v)   { return data = ((v & INVALID_ALL) << 4u) | (data & 0xf); }
-        auto value              (std::uint8_t v)    { return data = (data & (INVALID_ALL << 4u)) | (v & 0xf); }
-        auto empty              () const            { return value () != 0u || invalid () == INVALID_ALL; }
-        auto invalidate         (std::uint8_t v)    { if (v >= 1 && v <= 9) data |= (1 << (v + 3)); }
-        auto invalidate_set     (std::uint16_t v)   { data |= ((v & INVALID_ALL) << 4u); }
-        auto is_valid           (std::uint8_t v)    { return !value () && (v >= 1 && v <= 9) && !(data & (1 << (v + 3))); }
+        cell_type               (): _value (0u) {}
+        cell_type               (std::uint8_t v):_value (v), _mask (v == 0 ? 0 : INVALID_ALL) {}
+        cell_type& operator =   (std::uint8_t v)        { return *this = cell_type (v); }
+        auto invalid            () const                { return _mask; }
+        auto value              () const                { return _value; }
+        auto invalid            (std::uint16_t v)       { return _mask = v; }
+        auto value              (std::uint8_t v)        { return _mask = v == 0 ? 0u : INVALID_ALL, _value = v; }
+        auto is_set             () const                { return value () != 0u; }
+        auto empty              () const                { return !is_set () && invalid () == INVALID_ALL; }
+        auto& invalidate        (std::uint8_t v)        { if (v >= 1 && v <= 9) _mask |= (1 << (v - 1u)); return *this; }
+        auto& invalidate_set    (std::uint16_t v)       { _mask |= (v & INVALID_ALL); return *this; }
+        auto is_valid (std::uint8_t v) const            { return (v >= 1 && v <= 9) && ((is_set () && value () == v) || (_mask & (1 << (v - 1u)))); }
+
+        cell_type& operator = (const cell_type&) = default;
+        cell_type (const cell_type&) = default;
+
+        operator unsigned () const {
+            return value ();
+        }
      
     private:
-        std::uint16_t data;
+        std::uint8_t _value;
+        std::uint16_t _mask;
     };
     
     static inline auto map (std::uint32_t i, std::uint32_t j) {
@@ -80,7 +108,9 @@ struct sudoku_grid {
     auto&& cell (std::uint32_t i) const                     { return grid [i]; }
     auto&& cell (std::uint32_t i, std::uint32_t j)          { return grid [map (i, j)]; }
     auto&& cell (std::uint32_t i, std::uint32_t j) const    { return grid [map (i, j)]; }
-    auto good () { return is_good; }
+    auto is_good () const { return _is_good; }
+    auto good (bool b) { _is_good = b; }
+
 
     auto is_valid () const {
         for (auto i = 0u; i < 9u; ++i) {
@@ -121,29 +151,88 @@ protected:
         }
     }
 public:
+
+    auto mutate (std::uint32_t id, std::uint8_t v) const {
+        auto new_grid = *this;
+        new_grid.cell (id).value (v);
+        auto x = id%9u;
+        auto y = id/9u;
+        auto k = y - y%3u + x/3u;
+
+        for (auto i = 0u; i < 9u; ++i) {
+            auto _empty =
+                new_grid.cell (y*9u + i).invalidate (v).empty () ||
+                new_grid.cell (i*9u + x).invalidate (v).empty () ||
+                new_grid.cell (k, i).invalidate (v).empty ();
+            if (_empty) {
+                new_grid.good (false);
+                break;
+            }
+        }
+        return new_grid;
+    }
+
     sudoku_grid (const std::array<std::uint8_t, 81u>& in) {
         std::copy (std::begin (in), std::end (in), std::begin (grid));
         prune ();
     }
-    sudoku_grid () {}
+
+    sudoku_grid () {
+        std::fill (std::begin (grid), std::end (grid), 0u);
+    }
+
     sudoku_grid (const sudoku_grid&) = default;
+
     sudoku_grid& operator = (const sudoku_grid&) = default;
+
     sudoku_grid& operator = (const std::array<std::uint8_t, 81u>& in) {
         return *this = sudoku_grid (in);
     }
+
+    auto&& operator [] (std::uint32_t id)         { return cell (id); }
+    auto&& operator [] (std::uint32_t id) const   { return cell (id); }
+
+    operator bool () const { return is_good (); }
+
 private:
     std::array <cell_type, 81u> grid;
-    bool is_good = true;
+    bool _is_good = true;
 };
+
+bool solve (const sudoku_grid& grid, sudoku_grid& gout, std::uint32_t next = 0u) {
+    auto i = next;
+    while (i < 81u && grid.cell (i).is_set ()) ++i;
+    if (i >= 81u) {
+        gout = grid;
+        return true;
+    }
+    auto& _cell = grid.cell (i);
+    for (auto j = 0u; j < 9; ++j) {
+        if (!_cell.is_valid (j))
+            continue;
+        auto new_grid = grid.mutate (i, j);
+        if (!new_grid.is_good ())
+            continue;
+        if (solve (new_grid, gout, i + 1u))
+            return true;
+    }
+    return false;
+}
 
 
 int main () try {
-    auto puzzles = pre_load (std::cin);
-    sudoku_grid::cell_type cell;
-
-    
-
-    
+    auto puzzles = pre_load (std::cin, 50);
+    auto out = sudoku_grid ();
+    for (const auto& in: puzzles) {
+        print (in);
+        std::cout << "\n";
+        if (!solve (in, out)) {
+            std::cout << "Solution not found!\n";
+            continue;
+        }
+        print (out);
+        std::cout << "\n";
+    }
 
     return 0;
 }
