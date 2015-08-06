5bf5267689a9028da93a7f20a2b1ac3c4d7b9dce
Aleksandras <s.aleksandras@gmail.com>
2015-08-06 16:27:44 +0300

    refactoring code

--- Sudoku/Source.cpp
+++ Sudoku/Source.cpp
@@ -1,11 +1,161 @@
 #include <cstdint>
+#include <cassert>
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <array>
+#include <vector>
 #include <memory>
-#include <algorithm>
 #include <chrono>
 
+auto pre_load (std::istream& is, std::size_t n = 0u) {
+    static const auto bad_format =
+        std::runtime_error ("Bad input format");
+    typedef std::array<std::uint8_t, 81u> puzzle_grid_type;
+    static const auto load = [] (const std::string& line) {
+        puzzle_grid_type out;
+        std::transform (
+            std::begin (line), std::end (line),
+            std::begin (out), [] (auto x) {
+            return std::uint8_t (x == '.' ? 0u : x - '0');
+        });
+        return out;
+    };
+
+    std::string line;  
+    if (n > 0u) {
+        std::vector<puzzle_grid_type> out;
+        out.reserve (n);
+        for (auto i = 0u; std::getline (is, line) && i < n; ++i) {
+            if (line.size () != 81u)
+                throw bad_format;
+            out.emplace_back (load (line));
+        }
+        return out;
+    }
+    std::vector<puzzle_grid_type> out;
+    while (std::getline (is, line)) {
+        if (line.size () != 81u)
+            throw bad_format;
+        out.emplace_back (load (line));
+    }
+    return out;
+}
+
+struct sudoku_grid {    
+    struct cell_type {
+        static const auto INVALID_ALL = std::uint16_t (0x1ff);
+        cell_type               (): data (0u) {}
+        cell_type               (std::uint8_t v):data ((v&0xf) ? (v&0xf) | (INVALID_ALL << 4u) : (v&0xf)) {}
+        cell_type& operator =   (std::uint8_t v)    { return *this = cell_type (v); }
+        auto invalid            () const            { return (data >> 4u) & INVALID_ALL; }
+        auto value              () const            { return data & 0xf; }
+        auto invalid            (std::uint16_t v)   { return data = ((v & INVALID_ALL) << 4u) | (data & 0xf); }
+        auto value              (std::uint8_t v)    { return data = (data & (INVALID_ALL << 4u)) | (v & 0xf); }
+        auto empty              () const            { return value () != 0u || invalid () == INVALID_ALL; }
+        auto invalidate         (std::uint8_t v)    { if (v >= 1 && v <= 9) data |= (1 << (v + 3)); }
+        auto invalidate_set     (std::uint16_t v)   { data |= ((v & INVALID_ALL) << 4u); }
+        auto is_valid           (std::uint8_t v)    { return !value () && (v >= 1 && v <= 9) && !(data & (1 << (v + 3))); }
+     
+    private:
+        std::uint16_t data;
+    };
+    
+    static inline auto map (std::uint32_t i, std::uint32_t j) {
+        static const std::uint32_t mapping [9u] [9u] = {
+            { 0,  1,  2,  9, 10, 11, 18, 19, 20},
+            { 3,  4,  5, 12, 13, 14, 21, 22, 23},
+            { 6,  7,  8, 15, 16, 17, 24, 25, 26},
+            {27, 28, 29, 36, 37, 38, 45, 46, 47},
+            {30, 31, 32, 39, 40, 41, 48, 49, 50},
+            {33, 34, 35, 42, 43, 44, 51, 52, 53},
+            {54, 55, 56, 63, 64, 65, 72, 73, 74},
+            {57, 58, 59, 66, 67, 68, 75, 76, 77},
+            {60, 61, 62, 69, 70, 71, 78, 79, 80}
+        };    
+        return mapping [i] [j];
+    }
+
+    auto&& cell (std::uint32_t i)                           { return grid [i]; }
+    auto&& cell (std::uint32_t i) const                     { return grid [i]; }
+    auto&& cell (std::uint32_t i, std::uint32_t j)          { return grid [map (i, j)]; }
+    auto&& cell (std::uint32_t i, std::uint32_t j) const    { return grid [map (i, j)]; }
+    auto good () { return is_good; }
+
+    auto is_valid () const {
+        for (auto i = 0u; i < 9u; ++i) {
+            unsigned b [3u][9u] = {
+                {0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u},
+                {0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u},
+                {0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u}
+            };
+            for (auto j = 0u; j < 9u; ++j) {
+                if ((cell (j*9u + i).value () && ++b [0u] [cell (j*9u + i).value () - 1u] > 1u) ||
+                    (cell (i*9u + j).value () && ++b [1u] [cell (i*9u + j).value () - 1u] > 1u) ||
+                    (cell (i, j    ).value () && ++b [2u] [cell (i, j    ).value () - 1u] > 1u)) 
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+protected:
+    auto prune () {
+        sudoku_grid::cell_type row [9u], col [9u], blk [9u];
+        for (auto i = 0u; i < 9u; ++i) {
+            for (auto j = 0u; j < 9u; ++j) {
+                row [i].invalidate (cell (i*9u + j).value ());
+                col [i].invalidate (cell (j*9u + i).value ());
+                blk [i].invalidate (cell (map (i, j)).value ());
+            }
+        }
+
+        for (auto i = 0u; i < 9u; ++i) {
+            for (auto j = 0u; j < 9u; ++j) {
+                auto& rcell = cell (j, i);
+                rcell.invalidate_set (row [j].invalid ());
+                rcell.invalidate_set (col [i].invalid ());
+                rcell.invalidate_set (blk [i - i%3u + j/3u].invalid ());
+            }
+        }
+    }
+public:
+    sudoku_grid (const std::array<std::uint8_t, 81u>& in) {
+        std::copy (std::begin (in), std::end (in), std::begin (grid));
+        prune ();
+    }
+    sudoku_grid () {}
+    sudoku_grid (const sudoku_grid&) = default;
+    sudoku_grid& operator = (const sudoku_grid&) = default;
+    sudoku_grid& operator = (const std::array<std::uint8_t, 81u>& in) {
+        return *this = sudoku_grid (in);
+    }
+private:
+    std::array <cell_type, 81u> grid;
+    bool is_good = true;
+};
+
+
+int main () try {
+    auto puzzles = pre_load (std::cin);
+    sudoku_grid::cell_type cell;
+
+    
+
+    
+
+    return 0;
+}
+catch (const std::exception& e) {
+    std::cout << e.what () << "\n";
+    return -1;    
+}
+
+
+
+
+/*
 static const std::uint32_t mapping [9u] [9u] = {
     { 0,  1,  2,  9, 10, 11, 18, 19, 20},
     { 3,  4,  5, 12, 13, 14, 21, 22, 23},
@@ -237,3 +387,4 @@ catch (const std::exception& e) {
     std::cout << e.what () << "\n";
     return -1;
 }
+*/
